10dd1b354ee68c6c4ad3a5f60b7a4ac4a1331bcb
grahk93 <grace.juster@flatironschool.com>
2016-06-04 13:45:20 -0400

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
- # your code here!
+  i = 0
+  new_collection = []
+  while i < collection.length
+      new_collection << yield(collection[i])
+    i = i + 1
+  end
+  collection.select do |x|
+    x.even?
+  end
 end
+
